7e94fe1f360696dfe97874c3b9c2c52e9f6ab842
romi0330 <romina03villavicencio@gmail.com>
2023-05-27 12:43:37 -0500

    inicio de codigo

---
+++ proyecto.c
@@ -0,0 +1,17 @@
+/*En una Institución educativa se almacenan datos sobre el número de alumnos que han ingresado a sus
+diferentes carreras en los dos semestres anuales y en los últimos 5 años. Cada carrera tiene un 
+numero asociado que la identifica: 1. Ingeniería de Software. 2. Administración. 3. Economía.
+4. Relaciones Internacionales. 5. Matemáticas. 6. Contabilidad. 7. Ingeniería Industrial.
+Escriba un programa que calcule:
+a) El año en que ingresó la mayor cantidad de alumnos a la universidad.
+b) La Carrera que recibió la mayor cantidad de alumnos en el último año.
+c) ¿En qué año la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos?*/
+
+/*Anthony Almeida-Jeison Quimi-Romina Villavicencio*/
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    
+    return 0;
+}
