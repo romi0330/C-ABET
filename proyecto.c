33109b77fc33970427ba4eb3cde36c104a9ddc42
romi0330 <romina03villavicencio@gmail.com>
2023-05-28 12:42:48 -0500

    cambio de variables

--- proyecto.c
+++ proyecto.c
@@ -17,9 +17,9 @@ int main (int argc, char *argv[]) {
     //se utiliza srand para cada vez que se corre salgan numeros diferentes
     srand(time(NULL));
     //se declara variables por literales para mayor entendimiento
-    int suma, ingresos=0, año=0;
+    int suma, ingresos=0, anio=0;
     int alumnos=0, carrera=0;
-    int sof=0, años=0;
+    int sof=0, anios=0;
     int car[6][8];
     //se crea un ciclo anidado con el año y la carrera para determinar el ingreso de estudiantes
     for (int i = 1; i < 6; i++)
@@ -40,7 +40,7 @@ int main (int argc, char *argv[]) {
         if (j==1 && car[i][j] >sof)
         {
             sof=car[i][j];
-            años=i;
+            anios=i;
         }
         
         }
@@ -48,7 +48,7 @@ int main (int argc, char *argv[]) {
         if (suma>ingresos)
         {
             ingresos=suma;
-            año=i;
+            anio=i;
         }
         
     }
@@ -62,11 +62,11 @@ int main (int argc, char *argv[]) {
 
     }
     //se imprime los resultados de los literales
-    printf("a) El año en que ingresó la mayor cantidad de alumnos a la universidad fue el #%d con %d estudiantes \n", año, ingresos);
+    printf("a) El año en que ingresó la mayor cantidad de alumnos a la universidad fue el #%d con %d estudiantes \n", anio, ingresos);
     printf("b) La Carrera que recibió la mayor cantidad de alumnos en el último año fue la carrera #%d con %d estudiantes \n", carrera, alumnos);
     printf("LEYENDA DE CARRERAS \n");
     printf(" 1. Ingeniería de Software \n 2. Administración \n 3. Economía\n 4. Relaciones Internacionales\n 5. Matemáticas\n 6. Contabilidad\n 7. Ingeniería Industrial\n");
-    printf("c) El año en que la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos fue el #%d con %d estudiantes \n", años, sof);
+    printf("c) El año en que la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos fue el #%d con %d estudiantes \n", anios, sof);
     
     return 0;
 }
