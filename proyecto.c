78ac2d38073d3af5d6b8bae31d37766931230459
romi0330 <romina03villavicencio@gmail.com>
2023-05-27 23:34:31 -0500

    final todos los literales sin comentarios

--- proyecto.c
+++ proyecto.c
@@ -19,6 +19,7 @@ int main (int argc, char *argv[]) {
 
     int suma, ingresos=0, año=0;
     int alumnos=0, carrera=0;
+    int sof=0, años=0;
     int car[6][8];
 
     for (int i = 1; i < 6; i++)
@@ -33,6 +34,12 @@ int main (int argc, char *argv[]) {
             alumnos=car[i][j];
             carrera=j;
         }
+        if (j==1 && car[i][j] >sof)
+        {
+            sof=car[i][j];
+            años=i;
+        }
+        
 
         }
 
@@ -52,9 +59,11 @@ int main (int argc, char *argv[]) {
         }
         
     }
-    printf("a) El año en que ingresó la mayor cantidad de alumnos a la universidad fue el año #%d con %d estudiantes \n", año, ingresos);
-    printf("b) La Carrera que recibió la mayor cantidad de alumnos en el último año fue %d con %d estudiantes \n", carrera, alumnos);
-
+    printf("a) El año en que ingresó la mayor cantidad de alumnos a la universidad fue el #%d con %d estudiantes \n", año, ingresos);
+    printf("b) La Carrera que recibió la mayor cantidad de alumnos en el último año fue la carrera #%d con %d estudiantes \n", carrera, alumnos);
+    printf("LEYENDA DE CARRERAS \n");
+    printf(" 1. Ingeniería de Software \n 2. Administración \n 3. Economía\n 4. Relaciones Internacionales\n 5. Matemáticas\n 6. Contabilidad\n 7. Ingeniería Industrial\n");
+    printf("c) El año en que la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos fue el #%d con %d estudiantes \n", años, sof);
     
     return 0;
 }
